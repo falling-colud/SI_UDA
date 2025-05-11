1c9d20ad3886eaceea741c2f504a012f815fe124
MahmoudNazmi <mahmoodnazmii1516@gmail.com>
2025-05-12 00:49:56 +0200

    update2
    structure update , pagina centrale

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 //
 // Place any custom JS here
 //
+
+
+const target1 = document.getElementById('hover_target1');
+const section = document.getElementById('main');
+const target2 = document.getElementById("hover_target2")
+const immuneCell = document.getElementById("immuneCell")
+
+target1.addEventListener('mouseenter', () => {
+  section.classList.add('hovered1');
+});
+
+
+target1.addEventListener('mouseleave', () => {
+  section.classList.remove('hovered1');
+});
+
+target2.addEventListener('mouseenter' , () => {
+    section.classList.add("hovered2")
+})
+
+target2.addEventListener('mouseleave' , () => {
+    section.classList.remove('hovered2')
+})
